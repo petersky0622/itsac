eb21f6dc840b87b6b1edb23eb3506a7cfcc7706f
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-06-17 00:05:19 +0800

    Create [C_AR02-易] 一維陣列反轉 II.cpp

---
+++ [C_AR02-易] 一維陣列反轉 II.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;    
+    
+    
+int main(void){    
+    
+    int a[105];    
+    char b;    
+    int alen = 0;    
+    while(cin >> a[alen]){    
+        alen++;
+        if (cin.get() == '\n'){
+            
+            break;
+        }    
+    }    
+    
+    cout << a[alen - 1];    
+    for (int i = alen - 2; i >= 0; i--){    
+        cout << ' ' << a[i];    
+    }    
+    
+    cout << '\n';    
+    return 0;    
+}
