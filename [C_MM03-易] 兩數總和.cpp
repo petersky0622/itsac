17b311662105f300ad0d87f1d711b3ad486c6050
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-06-17 14:21:57 +0800

    Create [C_MM03-易] 兩數總和.cpp

---
+++ [C_MM03-易] 兩數總和.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+#include<iomanip> 
+using namespace std;
+  
+  
+
+  
+  
+  
+int main(void){      
+      
+    int a, b;  
+    while (cin >> a >> b){  
+    cout << a + b << endl;  
+    }  
+    return 0;      
+}  
