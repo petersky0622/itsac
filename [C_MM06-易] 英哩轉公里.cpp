c3697bb4f5d88c2fba8315b2f34cb30c90305b47
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-06-17 14:29:59 +0800

    Create [C_MM06-易] 英哩轉公里.cpp

---
+++ [C_MM06-易] 英哩轉公里.cpp
@@ -0,0 +1,13 @@
+#include<iostream>  
+#include<iomanip>    
+using namespace std;    
+    
+    
+    
+int main(void){        
+        
+    double a = 0;    
+    cin >> a;    
+    cout << fixed << setprecision(1) << a * 1.6 << '\n';    
+    return 0;        
+}
