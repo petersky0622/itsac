d64b7d733986c472ad28182228ac9738c21a40d5
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-06-17 15:18:37 +0800

    Create [C_MM08-易] 計算兩數和的平方值.cpp

---
+++ [C_MM08-易] 計算兩數和的平方值.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+#include<iomanip> 
+using namespace std;
+    
+    
+int main(void){      
+      
+    int a, b;  
+    cin >> a >> b;  
+    int c = a + b;  
+    cout << c * c << '\n';  
+    return 0;      
+}
