f1411c5b8ad3dc7da1273d774110ccbc1f6fc12a
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-06-17 14:19:08 +0800

    Create [C_MM02-易] 計算三角形面積.cpp

---
+++ [C_MM02-易] 計算三角形面積.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+#include<iomanip> 
+using namespace std;
+  
+  
+  
+int main(void){      
+      
+    float a, b;  
+    cin >> a >> b;  
+    cout << fixed << setprecision(1) << a * b / 2 << '\n';  
+    return 0;      
+} 
