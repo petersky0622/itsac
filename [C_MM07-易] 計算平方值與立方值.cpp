d5af81917cf8e7b57315e843388591e6d11f35c1
petersky0622 <101846262+petersky0622@users.noreply.github.com>
2023-06-17 15:12:17 +0800

    Create [C_MM07-易] 計算平方值與立方值.cpp

---
+++ [C_MM07-易] 計算平方值與立方值.cpp
@@ -0,0 +1,12 @@
+#include<iostream> 
+#include<iomanip> 
+using namespace std;
+    
+    
+int main(void){      
+      
+    int a;  
+    cin >> a;  
+    cout << a << ' ' << a * a << ' '<< a * a * a << '\n';  
+    return 0;      
+}
